fc98be8757eb12f83e9ab8dfa70edd753ccfdaab
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-02-13 11:11:24 +0100

    feat: add metadata configuration for improved SEO and page description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./global.css";
 import { RootProvider } from "fumadocs-ui/provider";
 import { Geist, Geist_Mono } from "next/font/google";
 import type { ReactNode } from "react";
+import { baseUrl, createMetadata } from "@/lib/metadata";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,11 +14,21 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export const metadata = createMetadata({
+  title: {
+    template: "%s | Learn The Web",
+    default: "Learn The Web",
+  },
+  description:
+    "A collection of docs, tutorials and resources to learn web development.",
+  metadataBase: baseUrl,
+});
+
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth`}
       suppressHydrationWarning
     >
       <body className="flex flex-col min-h-screen">
